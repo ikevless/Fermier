eef7a1f18ae816de6a52637304d935e242f5669b
kevin <ikevinhuerta@gmail.com>
2021-11-15 21:46:40 -0700

    MVC implementado

---
+++ Vistas/reg_pesaje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SG
+{
+    public partial class reg_pesaje : Form
+    {
+        public reg_pesaje()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_volver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void reg_pesaje_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Vistas/reg_vacuna.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SG
+{
+    public partial class reg_vacuna : Form
+    {
+        public reg_vacuna()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_volver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lbl_parto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
